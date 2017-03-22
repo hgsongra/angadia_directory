a1f971366e1da6fd963ca93734424008d014b028
Hitesh Songra <hgsongra@gmail.com>
2017-03-23 01:12:06 +0530

    Add more validation and footer

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,6 @@
 class Company < ApplicationRecord
 	has_many :contacts, dependent: :destroy
   validates_presence_of :name
+  validates_uniqueness_of :name
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ApplicationRecord
   belongs_to :company
   validates_presence_of :number, :city, :area
+  validates_uniqueness_of :number, scope: [:area, :city]
 end
