0943264327c10a1f6d9f63cff0d7ba1406cbf4bc
Hitesh Songra <hgsongra@gmail.com>
2017-03-22 23:42:00 +0530

    Add validation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,4 @@
 class Company < ApplicationRecord
+	has_many :contacts, dependent: :destroy
+  validates_presence_of :name
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,4 @@
 class Contact < ApplicationRecord
   belongs_to :company
+  validates_presence_of :number, :city
 end
