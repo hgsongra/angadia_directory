2b7b34b1341f07e0f831e55405797dcce5650b0f
Hitesh Songra <hgsongra@gmail.com>
2017-03-22 22:51:50 +0530

    Add header and made css modification

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,7 @@ class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.json
   def index
-    @contacts = Contact.all
+    @contacts = Contact.includes(:company).all
   end
 
   # GET /contacts/1
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'dashbord/index'
+  root 'dashbord#index'
 
   get 'dashbord/search'
 
