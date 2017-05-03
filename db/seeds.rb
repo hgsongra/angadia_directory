48c380b4f30c50476f811e6d2c046e7e3511222e
hgsongra <hgsongra@ymail.com>
2017-05-03 23:20:52 +0530

    Just remove Seed 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(email: "hgsongra15@gmail.com", password: "12345678", password_confirmation: "12345678")
-Admin.create!(email: "khamarbrijesh@gmail.com", password: "khamar@123", password_confirmation: "khamar@123")
